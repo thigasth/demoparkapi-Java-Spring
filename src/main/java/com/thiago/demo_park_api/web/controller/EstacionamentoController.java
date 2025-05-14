bfcd8959c1a18066963bbd0bcb1490abb2c4e0e4
thigasth <thigo830@gmail.com>
2025-05-14 20:05:21 -0300

    JasperSoftReports

--- src/main/java/com/thiago/demo_park_api/web/controller/EstacionamentoController.java
+++ src/main/java/com/thiago/demo_park_api/web/controller/EstacionamentoController.java
@@ -3,8 +3,10 @@ package com.thiago.demo_park_api.web.controller;
 import com.thiago.demo_park_api.entity.ClienteVaga;
 import com.thiago.demo_park_api.jwt.JwtUserDetails;
 import com.thiago.demo_park_api.repository.projection.ClienteVagaProjection;
+import com.thiago.demo_park_api.service.ClienteService;
 import com.thiago.demo_park_api.service.ClienteVagaService;
 import com.thiago.demo_park_api.service.EstacionamentoService;
+import com.thiago.demo_park_api.service.JasperService;
 import com.thiago.demo_park_api.web.dto.*;
 import com.thiago.demo_park_api.web.dto.mapper.ClienteVagaMapper;
 import com.thiago.demo_park_api.web.dto.mapper.PageableMapper;
@@ -17,6 +19,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -24,12 +27,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.IOException;
 import java.net.URI;
 
 import static io.swagger.v3.oas.annotations.enums.ParameterIn.PATH;
@@ -43,6 +48,9 @@ public class EstacionamentoController {
 
     private final EstacionamentoService estacionamentoService;
     private final ClienteVagaService clienteVagaService;
+    private final ClienteService clienteService;
+    private final JasperService jasperService;
+
 
 
     @Operation( summary = "Operacão de check-in", description = "Recurso para dar entrada de um veículo no estacionamento" +
@@ -200,6 +208,21 @@ public class EstacionamentoController {
         PageableDto dto = PageableMapper.toDto(projection);
         return ResponseEntity.ok(dto);
 
+    }
+
+    @GetMapping("/relatorio")
+    @PreAuthorize("hasRole('CLIENTE')")
+    public ResponseEntity<Void> getRelatorio(HttpServletResponse response, @AuthenticationPrincipal JwtUserDetails user) throws IOException {
+        String cpf = clienteService.buscarPorUsuarioId(user.getId()).getCpf();
+        jasperService.addParams("CPF", cpf);
+
+        byte[] bytes = jasperService.gerarPdf();
+
+        response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+        response.setHeader("Content-disposition", "inline; filename=" + System.currentTimeMillis() + ".pdf");
+        response.getOutputStream().write(bytes);
+
+        return ResponseEntity.ok().build();
     }
 
 }
--- src/main/java/com/thiago/demo_park_api/web/exception/ApiExceptionHandler.java
+++ src/main/java/com/thiago/demo_park_api/web/exception/ApiExceptionHandler.java
@@ -65,4 +65,16 @@ public class ApiExceptionHandler {
                 .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Campo(s) invalido(s)", result ));
     }
 
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorMessage> internalServerErrorException(Exception ex, HttpServletRequest request) {
+        ErrorMessage error = new ErrorMessage(
+                request, HttpStatus.INTERNAL_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+        log.error("Internal Server Error {} {}", error, ex);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(error);
+    }
+
 }
